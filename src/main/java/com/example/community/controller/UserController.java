d772571ec485fd191acf09aa1059576ffd7cfb36
changwooning <154579357+changwooning@users.noreply.github.com>
2025-09-10 13:23:45 +0900

    feature: UserController-signup 작성

---
+++ src/main/java/com/example/community/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.community.controller;
+
+import com.example.community.dto.UserRequestDto;
+import com.example.community.dto.UserResponseDto;
+import com.example.community.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/users")
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+  private final UserService userService;
+
+  // requestDto, responseDto 로 분리하기 -> service 수정
+  @PostMapping("/signup")
+  public ResponseEntity<UserResponseDto> signup(@RequestBody UserRequestDto userRequestDto) {
+    UserResponseDto savedUser = userService.signup(userRequestDto);
+    return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+  }
+
+
+}
--- src/main/java/com/example/community/service/UserService.java
+++ src/main/java/com/example/community/service/UserService.java
@@ -1,11 +1,14 @@
 package com.example.community.service;
 
+import com.example.community.dto.UserRequestDto;
+import com.example.community.dto.UserResponseDto;
 import com.example.community.entity.User;
 import com.example.community.enums.Role;
 import com.example.community.repository.UserRepository;
 import com.example.community.util.PasswordValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 @RequiredArgsConstructor
@@ -13,29 +16,36 @@ public class UserService {
 
   private final UserRepository userRepository;
 
-  public void signup(String userId, String password, String nickname) {
+  public UserResponseDto signup(UserRequestDto requestDto) {
     // 아이디 중복검사
-    if (userRepository.findByUserId(userId).isPresent()) {
+    if (userRepository.findByUserId(requestDto.getUserId()).isPresent()) {
       throw new IllegalArgumentException("이미 존재하는 아이디 입니다.");
     }
 
     // 닉네임 중복검사
-    if (userRepository.findByNickname(nickname).isPresent()) {
+    if (userRepository.findByNickname(requestDto.getNickname()).isPresent()) {
       throw new IllegalArgumentException("이미 존재하는 닉네임 입니다.");
     }
 
-    if (!PasswordValidator.isValid(password)) {
+    if (!PasswordValidator.isValid(requestDto.getPassword())) {
       throw new IllegalArgumentException("비밀번호는 10자리 이상, 영문+숫자 조합이여야 합니다.");
     }
 
     User user = User.builder()
-        .userId(userId)
-        .password(password)
-        .nickname(nickname)
+        .userId(requestDto.getUserId())
+        .password(requestDto.getPassword())
+        .nickname(requestDto.getPassword())
         .role(Role.USER)
         .build();
 
-    userRepository.save(user);
+    User savedUser = userRepository.save(user);
+
+    // User 엔티티 -> UserResponseDto 변환
+    return new UserResponseDto(
+        savedUser.getUserId(),
+        savedUser.getNickname(),
+        savedUser.getCreated_At()
+    );
 
   }
 
