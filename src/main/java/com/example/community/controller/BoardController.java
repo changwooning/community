c98750b576bca83cf699bbaabbdf42f7bd8050c6
changwooning <154579357+changwooning@users.noreply.github.com>
2025-09-25 13:47:21 +0900

    게시글 목록 조회 기능

--- src/main/java/com/example/community/controller/BoardController.java
+++ src/main/java/com/example/community/controller/BoardController.java
@@ -1,17 +1,24 @@
 package com.example.community.controller;
 
+import com.example.community.dto.BoardListResponseDto;
 import com.example.community.dto.BoardRequestDto;
 import com.example.community.dto.BoardResponseDto;
+import com.example.community.enums.SortType;
 import com.example.community.exception.InvalidBoardException;
 import com.example.community.service.BoardService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -36,6 +43,14 @@ public class BoardController {
     return ResponseEntity.status(HttpStatus.CREATED).body(response);
   }
 
+  @GetMapping("/listAll")
+  public ResponseEntity<BoardListResponseDto> getBoardList(
+      @PageableDefault(size = 5, sort = "createdAt", direction = Direction.DESC) Pageable pageable,
+      @RequestParam(defaultValue = "CREATED_AT") SortType sortBy) {
+
+    BoardListResponseDto response = boardService.getBoardList(pageable, sortBy);
+    return ResponseEntity.ok(response);
+  }
 
 
 }
--- src/main/java/com/example/community/repository/BoardRepository.java
+++ src/main/java/com/example/community/repository/BoardRepository.java
@@ -11,4 +11,6 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
   // 유저가 작성한 게시글 (페이징)
   Page<Board> findByUser(User user, Pageable pageable);
 
+  Page<Board> findAll(Pageable pageable);
+
 }
