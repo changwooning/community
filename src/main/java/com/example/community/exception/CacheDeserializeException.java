f4251411b571305511524bcdaa81c0f5fa8a9ed5
changwooning <154579357+changwooning@users.noreply.github.com>
2025-11-06 13:03:35 +0900

    feat: Redis 기반 조회수 캐시 및 스케줄러 적용

---
+++ src/main/java/com/example/community/exception/CacheDeserializeException.java
@@ -0,0 +1,7 @@
+package com.example.community.exception;
+
+public class CacheDeserializeException extends RuntimeException {
+  public CacheDeserializeException(String message) {
+    super(message);
+  }
+}
